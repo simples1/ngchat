5df9cfeda043a32cff6641446349a20898bd6a4f
simples1 <abdullaaden@outlook.com>
2018-04-09 05:42:39 +0100

    setup login area

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,23 @@
 angular.module('myApp', ['io.service']).
 
 run(function (io) {
-  io.init({
-    ioServer: 'http://localhost:3696'
-  });
 
-  io.subscribe()
 }).
 
 controller('MainController', function ($scope, io) {
+  $scope.hideChatArea = true;
+  $scope.hideLoginArea = false;
+
+  $scope.join = function () {
+    $scope.hideChatArea = false;
+    $scope.hideLoginArea = true;
+
+    io.init({
+      ioServer: 'http://localhost:3696'
+    });
+  }
+
+
 
   $scope.send = function () {
     io.emit({
@@ -20,7 +29,9 @@ controller('MainController', function ($scope, io) {
   io.watch('message', function (data) {
     console.log("watch",data)
     $scope.lastMessage = data.message;
+
     $("#chat").append("<div class='well'><div>"+data.message+"</div><div class='chattime'>"+ new Date().toUTCString() +"</div></div>");
+
     $scope.$apply();
   });
 });
